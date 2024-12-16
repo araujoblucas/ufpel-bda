4491d063cbe083d8aa728b9ce3e4058bf7f57f5f
Lucas Araujo <araujoblucas@gmail.com>
2024-12-16 09:04:34 -0300

    fix docker-compose and .envs

---
+++ database/migrations/2024_12_14_152403_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+            $table->boolean('is_active')->default(false);
+            $table->integer('quantity')->default(0);
+            $table->integer('quantity_available')->default(0);
+            $table->string('name');
+            $table->text('description');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
