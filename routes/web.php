b305dceea7df571331f2749c7e8b98c4bc472a03
Lucas Araujo <araujoblucas@gmail.com>
2025-02-24 19:42:35 -0300

    update routes and k6

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    $users = \App\Models\User::where("email", "like", "%a@e%")
+        ->orWhere("name", "like", "%Luc%")->limit(40)->get();
+
+    $cryptedUsers = [];
+    foreach ($users as $user) {
+        $cryptedUsers[]['name'] = bcrypt($user->name);
+        $cryptedUsers[]['email'] = bcrypt($user->email);
+    }
+
+    return response()->json($cryptedUsers);
+});
+
+Route::prefix('cached')->group(function () {
+    Route::get('/test', function () {
+        $encryptedUsers = cache()->remember('user_encrypted_data', 9999, function () {
+            $users = \App\Models\User::where("email", "like", "%a@e%")
+                ->orWhere("name", "like", "%Luc%")->limit(40)->get();
+
+            $cryptedUsers = [];
+            foreach ($users as $user) {
+                $cryptedUsers[]['name'] = bcrypt($user->name);
+                $cryptedUsers[]['email'] = bcrypt($user->email);
+            }
+
+            return $cryptedUsers;
+        });
+        return response()->json($encryptedUsers);
+    });
+});
+
--- scripts/test.js
+++ scripts/test.js
@@ -1,25 +1,15 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-
-export const options = {
-    vus: 100,
+import { check } from 'k6';
+export let options = {
+    vus: 10,
     duration: '30s',
 };
-
 export default function () {
     let res = http.get('http://nginx/test');
-
-    console.log(`Response: ${JSON.stringify(res)}`);
-
-    if (!res) {
-        console.error("Erro: resposta nula ou indefinida");
-        return;
-    }
-
+    // Basic checks on the HTTP response
     check(res, {
-        'status é 200': (r) => r.status === 200,
-        'responde rápido': (r) => r.timings.duration < 500,
+        'status is 200': (r) => r.status === 200,
+        'response time < 500ms': (r) => r.timings.duration < 500,
+        'content-type is JSON': (r) => r.headers['Content-Type'] === 'application/json',
     });
-
-    sleep(1);
 }
